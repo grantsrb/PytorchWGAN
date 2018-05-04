b3e2e275977c5efe299a2cd4cacf4c9e311b9fe7
Saetch Grant <grantsrb@gmail.com>
2018-05-03 19:00:05 -0700

    fix bugs, add hyperparameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torchvision
+import torch
 from gan import GAN
 from trainer import Trainer
 
@@ -8,7 +9,22 @@ def preprocess(imgs, mean, std):
 def postprocess(remakes, mean, std):
     return remakes*(std+1e-7) + mean
 
+def cuda_if(tobj):
+    if torch.cuda.is_available():
+        tobj = tobj.cuda()
+    return tobj
+
 if __name__ == "__main__":
+
+    save_root = "default"
+    n_epochs = 100000
+    n_critic = 5
+    batch_size = 64
+    lr = 5e-5
+    clip_coef = .01
+    emb_size = 200
+    process = False
+
     cifar = torchvision.datasets.CIFAR10("/Users/satchelgrant/Datasets/cifar10", train=True, download=True)
     imgs = cifar.train_data
     imgs = cuda_if(torch.FloatTensor(imgs.transpose((0,3,1,2))))
@@ -18,10 +34,22 @@ if __name__ == "__main__":
         imgs = preprocess(imgs, mean, std)
     
     gan = GAN(imgs.shape, emb_size=emb_size)
-    trainer = Trainer(gan, lr=lr, batch_size=batch_size, clip_coef=clip_coef, n_critic_loops=n_critic_loops)
-    
+    gan = cuda_if(gan)
+    trainer = Trainer(gan, lr=lr)
+    perm = cuda_if(torch.randperm(len(imgs)).long())
+    imgs = imgs.view(len(imgs), -1)
+    n_samples = n_critic*batch_size
+    counter = 0
     for epoch in range(n_epochs):
-        trainer.train()
+
+        if (counter+1)*n_samples > len(perm):
+            counter = 0
+            perm = cuda_if(torch.randperm(len(imgs)).long())
+        idxs = perm[counter*n_samples:(counter+1)*n_samples]
+        counter += 1
+
+        reals = imgs[idxs]
+        trainer.train(reals, batch_size=batch_size, clip_coef=clip_coef, n_critic=n_critic)
         trainer.print_statistics()
         if epoch % 10 == 0:
             n_imgs = 10
