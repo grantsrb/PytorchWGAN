7852519d0e537f07e2bd5af30d5361ef9b7236c6
Saetch Grant <grantsrb@gmail.com>
2018-05-11 10:55:16 -0700

    seperate learning rates for discriminator and generator, generate fakes by batch, add load model and get statistics functions

--- trainer.py
+++ trainer.py
@@ -9,25 +9,24 @@ class Trainer:
             tobj = tobj.cuda()
         return tobj
 
-    def __init__(self, gan, lr=1e-5):
+    def __init__(self, gan, gen_lr=5e-5, disc_lr=5e-5):
         self.gan = gan
-        self.disc_optim = optim.RMSprop(gan.discriminator.parameters(), lr=lr)
-        self.gen_optim = optim.RMSprop(gan.generator.parameters(), lr=lr)
+        self.disc_optim = optim.RMSprop(gan.discriminator.parameters(), lr=disc_lr)
+        self.gen_optim = optim.RMSprop(gan.generator.parameters(), lr=gen_lr)
         self.avg_real = 0
         self.avg_fake = 0
 
     def train(self, reals, batch_size=64, clip_coef=.01, n_critic=5):
-        fakes = self.gan.generate(len(reals))
-        fakes = fakes.data
         perm = self.cuda_if(torch.randperm(len(reals)).long())
 
         # Discrim Updating
         for i in range(n_critic):
+            fakes = self.gan.generate(batch_size)
             self.disc_optim.zero_grad()
 
             idxs = perm[i*batch_size:(i+1)*batch_size]
             rs = Variable(reals[idxs])
-            fs = Variable(fakes[idxs])
+            fs = Variable(fakes.data)
             r_loss = self.gan.discriminate(rs).mean()
             f_loss = self.gan.discriminate(fs).mean()
             disc_loss = f_loss - r_loss # Seek to maximize r_loss
@@ -47,6 +46,9 @@ class Trainer:
         f_loss.backward()
         self.gen_optim.step()
 
+    def get_statistics(self, epoch):
+        return self.avg_real, self.avg_fake
+        
     def print_statistics(self, epoch):
         diff = abs(self.avg_real-self.avg_fake)
         print("Epoch:", epoch, "– Avg Real:",self.avg_real, "– Avg Fake:",self.avg_fake,
@@ -57,6 +59,11 @@ class Trainer:
         torch.save(self.gan.state_dict(), save_root+"_gan.p")
         torch.save(self.disc_optim.state_dict(), save_root+"_d_optim.p")
         torch.save(self.gen_optim.state_dict(), save_root+"_g_optim.p")
+
+    def load_model(self, save_root):
+        self.gan.load_state_dict(torch.load(save_root+"_gan.p"))
+        self.disc_optim.load_state_dict(torch.load(save_root+"_d_optim.p"))
+        self.gen_optim.load_state_dict(torch.load(save_root+"_g_optim.p"))
 
     def get_imgs(self, n_imgs=10):
         imgs = self.fakes.data.cpu().numpy()
