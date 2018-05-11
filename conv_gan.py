6014017763d62f66f8d223a4650a2ab46ba629ae
Saetch Grant <grantsrb@gmail.com>
2018-05-11 10:53:51 -0700

    remove unnecessities, change name to DCGAN

--- conv_gan.py
+++ conv_gan.py
@@ -2,20 +2,17 @@ import torch.nn as nn
 from discriminator import Discriminator
 from generator import Generator
 
-class GAN(nn.Module):
+class DCGAN(nn.Module):
 
-    def __init__(self, img_shape, z_size=256, trainable_z=True, disc_bnorm=False, gen_bnorm=False):
+    def __init__(self, img_shape, z_size=100, trainable_z=False, disc_bnorm=True, gen_bnorm=True):
         """
         img_shape - the size of the input data. Shape = (..., C, H, W)
         """
-        super(GAN, self).__init__()
+        super(DCGAN, self).__init__()
         self.img_shape = img_shape
-        self.z_size = z_size
 
         self.discriminator = Discriminator(img_shape, bnorm=disc_bnorm)
-        self.flat_size = self.discriminator.flat_size
-        self.feat_shape = self.discriminator.feat_shape
-        self.generator = Generator(img_shape,self.flat_size, self.feat_shape, z_size=z_size, trainable_z=trainable_z, bnorm=gen_bnorm)
+        self.generator = Generator(img_shape, z_size=z_size, trainable_z=trainable_z, bnorm=gen_bnorm)
 
     def discriminate(self, x):
         return self.discriminator(x)
